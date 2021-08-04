e633133c7f4b8720ce5b1880bbfddd6761048907
jj131204 <jj131204@gmail.com>
2021-08-04 12:01:13 -0500

    test2

--- main.js
+++ main.js
@@ -11,12 +11,40 @@ class movies {
     this.title = arg[2];
     this.director = arg[3];
     this.year =  arg[4];
-
   }
 
+    add () {
+     // let new_array = [];
+      hola = hola.concat(movies);
+      return (new_array);
+    }
 }
 
-const test = new movies(arg[2], arg[3], arg[4]);
-console.log(test);
+let hola = [
+ {
+    title: 'Jurassic Park',
+    director: 'John Doe',
+    year: '1990'
+    },
+    {
+    title: 'The Dead Pool',
+    director: 'Mathew Albison',
+    year: '2014'
+    }
 
+];
+const test = new movies(arg[2], arg[3], arg[4]);
+	console.log (test);
+
+//if ()
+//const test = new movies(arg[2], arg[3], arg[4]);
+
+/*let new_array = [];
+//console.log(test);
+
+new_array = hola.concat(test);
+
+
+console.log(new_array);
+*/
 //module.exports = movies;
